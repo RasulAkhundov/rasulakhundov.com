8d5bd24ab415f38fada2772c779742554ece9763
“Resulaxundov” <resul.axundov2002@mail.ru>
2022-07-02 17:36:15 +0400

    new changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                </div>
                <div className="col-12 col-lg-6 d-flex flex-column justify-content-center" data-aos="zoom-in" data-aos-duration="1000">
                   <div className="about-content">
-                     <h2>Hello, Im Rasul Akhundov</h2>
+                     <h2>Hi, Im Rasul Akhundov</h2>
                      <p>Front End developer with industry experience building websites and web applications. I specialize in JavaScript. Experience working with Node Js and Mongo DB. Also have proffesional experience working with Scss BootStrap Jquery RestApi and React</p>
                      <a href="https://docs.google.com/uc?export=download&id=19SC5dBuz_HjDmTRuu_Kg8LugEU-ER-XJ">
                         <button type='button'><i className='fa-solid fa-download'></i> DOWNLOAD CV</button>
@@ -30,10 +30,6 @@ const About = () => {
                      <div className="row">
                         <div className="col-12 col-md-6 mb-4 mb-md-0">
                            <ul>
-                              <li>
-                                 <span>Birthday:</span>
-                                 <span>19/10/2002</span>
-                              </li>
                               <li>
                                  <span>City:</span>
                                  <span>Baku, Azerbaijan</span>
@@ -59,11 +55,7 @@ const About = () => {
                                  <span>Baku Computer Vocational College</span>
                               </li>
                               <li>
-                                 <span>Website:</span>
-                                 <span>www.rasulakhundov.com</span>
-                              </li>
-                              <li>
-                                 <span>Phone:</span>
+                                 <span>Number:</span>
                                  <span>+994555074498</span>
                               </li>
                            </ul>
